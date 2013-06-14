d836ec08490cdfecea66a3e5b331e4df298d297d
Neil Dey <nilashis@gmail.com>
2013-06-14 04:13:40 -0500

    started location.java - incomplete.
    Issue: Cannot import "import com.google.android.gms.*"

---
+++ MyAndroidExperiments/src/main/java/com/myandroidexperiments/me/location.java
@@ -0,0 +1,24 @@
+package com.myandroidexperiments.me;
+
+import android.support.v4.app.FragmentActivity;
+
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+import android.location.Location;
+
+
+/**
+ * Created by home on 6/14/13.
+ */
+public class location extends FragmentActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
